9ca19f8ce508f2be61c7efb42e8ce4e89ce5e490
darthjee favini <darthjee@gmail.com>
2015-06-07 01:40:34 -0300

    Refactor spec

--- spec/lib/json_parser/post_processor_spec.rb
+++ spec/lib/json_parser/post_processor_spec.rb
@@ -45,30 +45,23 @@ describe JsonParser::PostProcessor do
       let(:value) { '1' }
       let(:options) { { type: type } }
 
-      context 'with integer type' do
-        let(:type) { :integer }
+      shared_context 'a result that is type cast' do |types|
+        types.each do |type, clazz|
+          context "with #{type} type" do
+            let(:type) { type }
 
-        it do
-          expect(result).to be_a(Integer)
+            it do
+              expect(result).to be_a(clazz)
+            end
+          end
         end
       end
 
-      context 'with float type' do
-        let(:type) { :float }
-
-        it do
-          expect(result).to be_a(Float)
-        end
-      end
-
-      context 'with string type' do
-        let(:value) { 1 }
-        let(:type) { :string }
-
-        it do
-          expect(result).to be_a(String)
-        end
-      end
+      it_behaves_like 'a result that is type cast', {
+        integer: Integer,
+        float: Float,
+        string: String
+      }
     end
   end
 end
