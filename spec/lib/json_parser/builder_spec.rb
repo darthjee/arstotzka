7df061267e1921d33bf94fba0c48911112b26c1a
darthjee <darthjee@gmail.com>
2018-05-23 22:18:56 +0200

    Add more specs

--- spec/lib/json_parser/builder_spec.rb
+++ spec/lib/json_parser/builder_spec.rb
@@ -1,7 +1,15 @@
 require 'spec_helper'
 
 describe JsonParser::Builder do
-  let(:clazz) { Class.new }
+  let(:clazz) do
+    Class.new.tap do |c|
+      c.send(:attr_reader, :json)
+      c.send(:define_method, :initialize) do |json={}|
+        @json = json
+      end
+    end
+  end
+
   let(:options) { {} }
   let(:attr_name) { :name }
   let(:attr_names) { [ attr_name ] }
@@ -11,10 +19,44 @@ describe JsonParser::Builder do
   end
 
   describe '#build' do
-    it do
+    it 'adds the reader' do
       expect do
         subject.build
       end.to change { clazz.new.respond_to?(attr_name) }
+    end
+
+    context 'when building several attributes' do
+      let(:json) { {} }
+      let(:attr_names) { [ :id, :name, :age ] }
+      let(:instance) { clazz.new(json) }
+
+      before { subject.build }
+
+      it 'adds all the readers' do
+        attr_names.each do |attr|
+          expect(instance).to respond_to(attr)
+        end
+      end
+
+      it 'fetches safelly empty jsons' do
+        expect(instance.name).to be_nil
+      end
+
+      context 'when json has the property' do
+        let(:json) { { name: 'Robert' } }
+
+        it 'fetches safelly empty jsons' do
+          expect(instance.name).to eq('Robert')
+        end
+
+        context 'but key is a string' do
+          let(:json) { { 'name' => 'Robert' } }
+
+          it 'fetches safelly empty jsons' do
+            expect(instance.name).to eq('Robert')
+          end
+        end
+      end
     end
   end
 end
