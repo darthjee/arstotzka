c7fe00ce29cc6f661b3587419f0e723db9977c7e
darthjee <darthjee@gmail.com>
2018-05-24 13:10:15 +0200

    Add spec over class wrapping

--- spec/lib/json_parser/builder_spec.rb
+++ spec/lib/json_parser/builder_spec.rb
@@ -11,6 +11,7 @@ describe JsonParser::Builder do
   end
 
   let(:options) { {} }
+  let(:name) { 'Robert' }
   let(:attr_name) { :name }
   let(:attr_names) { [ attr_name ] }
   let(:json) { {} }
@@ -27,57 +28,71 @@ describe JsonParser::Builder do
       end.to change { clazz.new.respond_to?(attr_name) }
     end
 
-    context 'when building several attributes' do
-      let(:attr_names) { [ :id, :name, :age ] }
-
+    context 'after building' do
       before { subject.build }
 
-      it 'adds all the readers' do
-        attr_names.each do |attr|
-          expect(instance).to respond_to(attr)
-        end
-      end
+      context 'when building several attributes' do
+        let(:attr_names) { [ :id, :name, :age ] }
 
-      it 'fetches safelly empty jsons' do
-        expect(instance.name).to be_nil
-      end
-
-      context 'when json has the property' do
-        let(:json) { { name: 'Robert' } }
-
-        it 'fetches the value' do
-          expect(instance.name).to eq('Robert')
+        it 'adds all the readers' do
+          attr_names.each do |attr|
+            expect(instance).to respond_to(attr)
+          end
         end
 
-        context 'but key is a string' do
-          let(:json) { { 'name' => 'Robert' } }
+        it 'fetches safelly empty jsons' do
+          expect(instance.name).to be_nil
+        end
+
+        context 'when json has the property' do
+          let(:json) { { name: name } }
 
           it 'fetches the value' do
-            expect(instance.name).to eq('Robert')
+            expect(instance.name).to eq(name)
+          end
+
+          context 'but key is a string' do
+            let(:json) { { 'name' => name } }
+
+            it 'fetches the value' do
+              expect(instance.name).to eq(name)
+            end
           end
         end
       end
-    end
 
-    context 'when value is deep within the json' do
-      let(:json) { { user: { name: 'Robert' } } }
+      context 'when value is deep within the json' do
+        let(:json) { { user: { name: name } } }
 
-      before { subject.build }
+        context 'when defining a path' do
+          let(:options) { { path: 'user' } }
 
-      context 'when defining a path' do
-        let(:options) { { path: 'user' } }
+          it 'fetches the value within the json' do
+            expect(instance.name).to eq(name)
+          end
+        end
 
-        it 'fetches the value within the json' do
-          expect(instance.name).to eq('Robert')
+        context 'when defining a fullpath' do
+          let(:options) { { full_path: 'user.name' } }
+          let(:attr_name) { :the_name }
+
+          it 'fetches the value within the json' do
+            expect(instance.the_name).to eq(name)
+          end
         end
       end
 
-      context 'when defining a fullpath' do
-        let(:options) { { full_path: 'user.name' } }
-        let(:attr_name) { :the_name }
+      context 'when wrapping with a class' do
+        let(:json) { { person: name } }
+        let(:options) { { class: JsonParser::Person } }
+        let(:attr_name) { :person }
 
-        it 'fetches the value within the json' do
-          expect(instance.the_name).to eq('Robert')
+        it do
+          expect(instance.person).to be_a(JsonParser::Person)
+        end
+
+        it 'fills the new instance with the information fetched' do
+          expect(instance.person.name).to eq(name)
         end
       end
     end
