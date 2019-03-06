d925a4f44a5151a6e3d5f88d8c52dba0b1bbd6f8
darthjee <darthjee@gmail.com>
2019-03-06 14:27:03 +0100

    Add documentation to wrapper

--- lib/arstotzka/wrapper.rb
+++ lib/arstotzka/wrapper.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
 module Arstotzka
+  # @api private
+  #
   # Class responsible for wrapping / parsing a value fetched
   class Wrapper
     include Arstotzka::TypeCast
 
+    # Returns a new instance of Wrapper
+    #
     # @param clazz [Class] class to wrap the value
     # @param type [String/Symbol] type to cast the value. The
     #   possible type_cast is defined by {TypeCast}
@@ -37,8 +41,14 @@ module Arstotzka
 
     private
 
+    # @private
     attr_reader :clazz, :type
 
+    # @private
+    #
+    # Wraps an element with a class and perform typecasting
+    #
+    # @return [Object]
     def wrap_element(value)
       value = cast(value) if type? && !value.nil?
       return if value.nil?
@@ -46,14 +56,33 @@ module Arstotzka
       clazz ? clazz.new(value) : value
     end
 
+    # @private
+    #
+    # Wraps each element of the array
+    #
+    # @see #wrap_element
+    #
+    # @return [Arra]
     def wrap_array(array)
       array.map { |v| wrap v }
     end
 
+    # @private
+    #
+    # Check if type was given
+    #
+    # @return [Boolean]
     def type?
       type.present? && type != :none
     end
 
+    # @private
+    #
+    # Performs type casting
+    #
+    # @see Arstotzka::TypeCaster
+    #
+    # @return [Object]
     def cast(value)
       public_send("to_#{type}", value)
     end
