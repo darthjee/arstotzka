d561ccb4ca608bfea2885ff550f9b759fc0bd618
darthjee <darthjee@gmail.com>
2019-03-11 15:45:09 +0100

    Add Base documentation

--- lib/arstotzka/base.rb
+++ lib/arstotzka/base.rb
@@ -1,9 +1,21 @@
 # frozen_string_literal: true
 
 module Arstotzka
+  # @api private
+  #
+  # Common code to be used by classes
   module Base
     private
 
+    # Sets the @options object
+    #
+    # @overload options=(options_hash={})
+    #   @param options_hash [Hash] options
+    #
+    # @overload options=(options)
+    #   @param options [Arstotzka::Options] options object
+    #
+    # @return [Arstotzka::Options]
     def options=(options)
       @options = if options.is_a?(Hash)
                    Options.new(options)
