4c8afe90ad785b12ccc35d056f17f486e3e6d3e5
darthjee <darthjee@gmail.com>
2019-07-11 18:05:18 +0200

    Declare options hash only once

--- lib/arstotzka/class_methods.rb
+++ lib/arstotzka/class_methods.rb
@@ -73,12 +73,16 @@ module Arstotzka
     # @param options_hash [Hash] exposing options
     #
     # @option options_hash after [String,Symbol] instance method to be called on the
-    #   returning value. The result of the method call will be the actual value
+    #   returning value returned by {Crawler} before being returned by {Fetcher}.
+    #   The result of the method call will be the actual value
     #   (used by {PostProcessor})
+    #
     # @option options_hash after_each [String,Symbol] instance method to be
     #   called on each element found when crawling the hash.
     #   If an array should be returned, after_each will be called on each individual
-    #   elements (even nil elements) (used by {Wrapper})
+    #   elements (even nil elements) while +after+ is only called on the
+    #   collection. (used by {Wrapper})
+    #
     # @option options_hash cached [Boolean,Symbol] Flag to ensure
     #   there value is cached after first fetching.
     #   (used by {Fetcher::Cache})
@@ -87,27 +91,40 @@ module Arstotzka
     #   - +true+ : Simple cache, nil values are always considered
     #     not cached
     #   - +:full+ : cache even nil values
-    # @option options_hash case [Symbol] case type of the keys (used by {Crawler})
+    #
+    # @option options_hash case [Symbol] case type of the keys in the hash
+    #   (used by {Reader})
     #   - +:snake+ : snake_cased keys
     #   - +:lower_camel+ : lowerCamelCased keys
     #   - +:upper_camel+ : UperCamelCased keys
+    #
     # @option options_hash compact [Boolean] flag signallying if nil values should be
-    #   removed of array (used by {Crawler})
+    #   removed of array (applying +Array#compact+) (used by {Crawler})
+    #
     # @option options_hash default [Object] default value to be returned when failing
     #   to fetch a value (used by {Crawler})
-    # @option options_hash flatten [Boolean] full path of keys for
-    #   fetching the value (eg. +'person.name'+) (used by {PostProcessor})
+    #
+    # @option options_hash flatten [Boolean] flag signallying if nil values should be
+    #   removed of array (applying +Array#flatten+) (used by {PostProcessor})
+    #
     # @option options_hash full_path [String] full path of keys to be used in when
     #   not wanting to have the attribute defined as key. (eg. +'person.name'+)
     #   (used by {Crawler})
+    #
     # @option options_hash klass [Class] class to wrap the value (used by {Wrapper})
+    #
     # @option options_hash json [String,Symbol] name of the method or variable
     #   used to fetch the hash to be crawled (eg. +:hash+, +:@hash+, +"@@config"+)
     #   (used by {HashReader})
+    #
     # @option options_hash path [String,Symbol] base path to be crawled.
     #   (used by {Crawler})
+    #
     # @option options_hash type [String,Symbol] type to cast the value. The
     #   possible type_cast is defined by {TypeCast} (used by {Wrapper})
+    #   - +integer+
+    #   - +string+
+    #   - +float+
     #
     # @example
     #   class MyModel
