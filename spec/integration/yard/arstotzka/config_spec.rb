cd4af4c11b00cb34de0638545ba9f2841f60b4d0
darthjee <darthjee@gmail.com>
2019-07-12 16:56:12 +0200

    Add example of configuration

--- spec/integration/yard/arstotzka/config_spec.rb
+++ spec/integration/yard/arstotzka/config_spec.rb
@@ -6,6 +6,41 @@ describe Arstotzka::Config do
   describe 'yard' do
     subject(:config) { Arstotzka.config }
 
+    after { Arstotzka.reset_config }
+
+    describe 'Redefining json method and case type' do
+      let(:office) { Office.new(hash) }
+
+      let(:hash) do
+        {
+          employes: [{
+            first_name: 'Rob'
+          }, {
+            first_name: 'Klara'
+          }]
+        }
+      end
+
+      let(:error_message) do
+        "undefined method `json' for #{office}\nDid you mean?  JSON"
+      end
+
+      it do
+        expect { office.employes }
+          .to raise_error(NoMethodError, error_message)
+      end
+
+      context 'when json option is defined' do
+        before { Arstotzka.configure { json :@hash } }
+
+        it do
+          expect { Arstotzka.configure { |c| c.case :snake } }
+            .to change(office, :employes)
+            .from([]).to(%w[Rob Klara])
+        end
+      end
+    end
+
     describe '#options' do
       let(:overrides) { { klass: Person } }
 
@@ -29,8 +64,6 @@ describe Arstotzka::Config do
           config.case :snake
         end
       end
-
-      after { Arstotzka.reset_config }
 
       it 'configure options to use snake case' do
         expect(config.options(overrides).to_h).to eq(expected)
