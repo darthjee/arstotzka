fae0362bcd3606328fbe47dfbc85d22ae43d5f15
darthjee <darthjee@gmail.com>
2019-07-12 20:02:55 +0200

    Add missing file

---
+++ spec/support/models/office.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class Office
+  include Arstotzka
+
+  expose :employes, full_path: 'employes.first_name',
+                    compact: true
+
+  def initialize(hash)
+    @hash = hash
+  end
+end
